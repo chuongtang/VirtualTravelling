dbc5fba5a9c404c5595096a60ba5b69925f017d0
Chuong Tang <chuong.tang@aol.com>
2021-04-21 14:27:51 -0700

    Final upload
    Final upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// import { func } from 'joi';
 import { fetchApi, postApi, delApi, updateApi } from './apiMethods.js';
 
 const menuBars = document.getElementById('menu-bars');
@@ -7,7 +8,8 @@ const dirOut = 'out';
 const navItemNum = [1, 2, 3, 4, 5];
 const navArray = [];
 navItemNum.map(i => navArray.push(document.getElementById(`nav${i}`)));
-
+const loader = document.getElementById('lds-dual-ring');
+const mainContainer = document.getElementById('mainContainer');
 // Start the page animation codes
 function sliding(dir1, dir2) {
     overlay.classList.remove(`overlay-slide-${dir2}`);
@@ -31,26 +33,49 @@ function toggleNav() {
 
 
 const apiUrl = 'https://c6project1api.herokuapp.com/api/memories';
-
-// const apiEndPoint = `${apiUrl}/memory.id`
-// const displayCardIds = [];
 const fetchButton = document.getElementById('fetchButton');
 const postButton = document.getElementById('postButton');
 const memoryCard = document.getElementById('memoryCard');
+const memoryForm = document.getElementById('memoryForm');
+const postTitle = document.getElementById('postTitle');
+const editTitle = document.getElementById('editTitle');
+const updateButton = document.getElementById('updateButton');
 
-// FetchApi();   /// Onload 
+
 
 // *************** ⤴️ good codes ❤
 
+// Show Loading animation //
+function showLoadingSpinner(){
+    loader.hidden = false;
+    fetchButton.hidden = true;
+    mainContainer.hidden = true;
+}
+
+// Hide loading animation and show quoteContainer //
+function removeLoadingSpinner(){
+    if(!loader.hidden){
+        mainContainer.hidden = false;
+        fetchButton.hidden = false;
+        loader.hidden = true;
+    }
+}
+
 //**************** ⤵️ woking codes 🔥
-
-
+// 🧹Clearing form after editting
+function clearForm() {
+    memoryForm.name.value = ''; 
+    memoryForm.place.value = ''; 
+    memoryForm.travelDate.value = ''; 
+    memoryForm.tags.value = ''; 
+    memoryForm.message.value = '';
+    postTitle.hidden = false;
+    postButton.hidden = false; 
+    editTitle.hidden = true;
+    updateButton.hidden = true; 
+}
+// 🖮 Edit post  
 function editMemory(params) {
-    const memoryForm = document.getElementById('memoryForm');
-    const postTitle = document.getElementById('postTitle');
-    const editTitle = document.getElementById('editTitle');
-    const postButton = document.getElementById('postButton');
-    const updateButton = document.getElementById('updateButton');
     memoryForm.name.value = params.name; 
     memoryForm.place.value = params.place; 
     memoryForm.travelDate.value = params.travelDate; 
@@ -60,13 +85,6 @@ function editMemory(params) {
     postButton.hidden = true; 
     editTitle.hidden = false;
     updateButton.hidden = false; 
-    // const newMemory = {
-    //     "name": `${memoryForm.name.value}`,
-    //     "place": `${memoryForm.place.value}`,
-    //     "travelDate": `${memoryForm.travelDate.value}`,
-    //     "tags": `${memoryForm.tags.value}`,
-    //     "message": `${memoryForm.message.value}`,
-    // }
     updateButton.addEventListener('click', function (e) {
         console.log(`${params.name}'s memory will be updated`);
         const newMemory = {
@@ -79,13 +97,16 @@ function editMemory(params) {
         console.log(newMemory);
         e.preventDefault();
         updateApi(params.id, newMemory);
-        alert(`${params.name}'s memory  with ID ${params.id} has been updated. Fetch again to see new cards`);
+        alert(`${params.name}'s memory  with ID ${params.id} has been updated.
+        Fetch again to see new cards`);
+        clearForm();
     });
 }
 
 
 // 🖥️ Create cards from Get/API JSON and DISPLAY to home page
 async function displayMemories() {
+    showLoadingSpinner();
     const memoriesDiv = document.querySelector('#parentcard');
     memoriesDiv.innerText = '';
     let memories = await fetchApi();
@@ -95,15 +116,15 @@ async function displayMemories() {
         memoryElement.classList.add(`div${index + 1}`, `memoryCard`);
         memoryElement.setAttribute('id', memory.id);
         memoryElement.innerText = `
-            Name: ${memory.name}  
-            Place: ${memory.place}  
-            TravelDate: ${memory.travelDate}  
-            Tags: ${memory.tags}  
-            
-            Message: ${memory.message}
-            
-            `;
-
+        Name: ${memory.name}  
+        Place: ${memory.place}  
+        TravelDate: ${memory.travelDate}  
+        Tags: ${memory.tags}  
+        
+        Message: ${memory.message}
+        
+        `;
+        
         const delButton = document.createElement('Button');
         delButton.innerText = `Delete`;
         delButton.classList.add('delButton');
@@ -113,7 +134,7 @@ async function displayMemories() {
             e.preventDefault();
             delApi(this.value);
         });
-
+        
         const editButton = document.createElement('Button');
         editButton.classList.add('editButton');
         editButton.innerText = `Edit`;
@@ -127,6 +148,8 @@ async function displayMemories() {
         memoryElement.append(editButton, delButton);
         memoriesDiv.append(memoryElement,);
     });
+    removeLoadingSpinner();
+    clearForm();
 }
 
 //🖅 Onclick function for submit button to send POST  request
@@ -144,51 +167,21 @@ function postExperience(e) {
         "message": `${message}`,
     }
     console.log(newPost);
-    postApi(newPost);
-}
-
-
-
-// 🖮 Edit post  
-async function editPost(postId) {
-    // send all the cards info to .createCardContainer
-    memoryCard.elements['name'].value = `${memory.name}`;
-    memoryCard.elements['place'].value = `${memory.place}`;
-    memoryCard.elements['travelDate'].value = `${memory.travelDate}`;
-    memoryCard.elements['tags'].value = `${memory.tags}`;
-    memoryCard.elements['message'].value = `${memory.message}`;
-    // delete old post info
-    const deleteEndpoint = `${apiUrl}/${postId}`;
-    try {
-        const response = await fetch(deleteEndpoint, {
-            method: 'delete',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({ id: `${postId}` }),
-        })
-        const data = await response.json();
-        alert(`${data.name} memory is ready to be editted`);
-        //   FetchApi();
+    const newPostArray = Object.values(newPost);
+    if(newPostArray.includes('')){
+        alert(`All fields are required🤔`)
         return;
-    } catch (error) {
-        console.log(error);
+    } else {
+        postApi(newPost);
     }
+    
 }
 
 
-
-
-
-
-
-
-//  Event listeners for the menu
+//  🞜Event listeners for the menu
 menuBars.addEventListener('click', toggleNav);
 navArray.forEach(element => element.addEventListener('click', toggleNav));
 
-//  Event listeners for the API routes
+//   🞜Event listeners for the API routes
 fetchButton.addEventListener('click', displayMemories);
 postButton.addEventListener('click', postExperience);
-
-
-// delPostOnClick(1);
-// delApi(3);
